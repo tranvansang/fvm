1fe5ebfe979f7109eb14491ff9bfd5c29e488387
Tran Sang <tranvansangk41@gmail.com>
2018-07-28 21:29:22 +0900

    add all-sleep.sh

---
+++ L3/run/all-sleep.sh
@@ -0,0 +1,215 @@
+set -x
+qsub go0-1.sh
+sleep 480
+rbstat
+qsub go2-1.sh
+sleep 480
+rbstat
+qsub go0-2.sh
+sleep 420
+rbstat
+qsub go2-2.sh
+sleep 420
+rbstat
+qsub go0-3.sh
+sleep 240
+rbstat
+qsub go2-3.sh
+sleep 240
+rbstat
+qsub go0-4.sh
+sleep 240
+rbstat
+qsub go2-4.sh
+sleep 240
+rbstat
+qsub go0-5.sh
+sleep 180
+rbstat
+qsub go2-5.sh
+sleep 180
+rbstat
+qsub go0-6.sh
+sleep 180
+rbstat
+qsub go2-6.sh
+sleep 180
+rbstat
+qsub go0-7.sh
+sleep 180
+rbstat
+qsub go2-7.sh
+sleep 180
+rbstat
+qsub go0-8.sh
+sleep 180
+rbstat
+qsub go2-8.sh
+sleep 180
+rbstat
+qsub go0-9.sh
+sleep 180
+rbstat
+qsub go2-9.sh
+sleep 180
+rbstat
+qsub go0-10.sh
+sleep 180
+rbstat
+qsub go2-10.sh
+sleep 180
+rbstat
+qsub go0-11.sh
+sleep 180
+rbstat
+qsub go2-11.sh
+sleep 180
+rbstat
+qsub go0-12.sh
+sleep 180
+rbstat
+qsub go2-12.sh
+sleep 180
+rbstat
+qsub go0-13.sh
+sleep 180
+rbstat
+qsub go2-13.sh
+sleep 180
+rbstat
+qsub go0-14.sh
+sleep 180
+rbstat
+qsub go2-14.sh
+sleep 180
+rbstat
+qsub go0-15.sh
+sleep 180
+rbstat
+qsub go2-15.sh
+sleep 180
+rbstat
+qsub go0-16.sh
+sleep 180
+rbstat
+qsub go2-16.sh
+sleep 180
+rbstat
+qsub go0-17.sh
+sleep 180
+rbstat
+qsub go2-17.sh
+sleep 180
+rbstat
+qsub go0-18.sh
+sleep 180
+rbstat
+qsub go2-18.sh
+sleep 180
+rbstat
+qsub go0-19.sh
+sleep 180
+rbstat
+qsub go2-19.sh
+sleep 180
+rbstat
+qsub go0-20.sh
+sleep 180
+rbstat
+qsub go2-20.sh
+sleep 180
+rbstat
+qsub go0-21.sh
+sleep 180
+rbstat
+qsub go2-21.sh
+sleep 180
+rbstat
+qsub go0-22.sh
+sleep 180
+rbstat
+qsub go2-22.sh
+sleep 180
+rbstat
+qsub go0-23.sh
+sleep 180
+rbstat
+qsub go2-23.sh
+sleep 180
+rbstat
+qsub go0-24.sh
+sleep 180
+rbstat
+qsub go2-24.sh
+sleep 180
+rbstat
+qsub go0-25.sh
+sleep 180
+rbstat
+qsub go2-25.sh
+sleep 180
+rbstat
+qsub go0-26.sh
+sleep 180
+rbstat
+qsub go2-26.sh
+sleep 180
+rbstat
+qsub go0-27.sh
+sleep 180
+rbstat
+qsub go2-27.sh
+sleep 180
+rbstat
+qsub go0-28.sh
+sleep 180
+rbstat
+qsub go2-28.sh
+sleep 180
+rbstat
+qsub go0-29.sh
+sleep 180
+rbstat
+qsub go2-29.sh
+sleep 180
+rbstat
+qsub go0-30.sh
+sleep 180
+rbstat
+qsub go2-30.sh
+sleep 180
+rbstat
+qsub go0-31.sh
+sleep 180
+rbstat
+qsub go2-31.sh
+sleep 180
+rbstat
+qsub go0-32.sh
+sleep 180
+rbstat
+qsub go2-32.sh
+sleep 180
+rbstat
+qsub go0-33.sh
+sleep 180
+rbstat
+qsub go2-33.sh
+sleep 180
+rbstat
+qsub go0-34.sh
+sleep 180
+rbstat
+qsub go2-34.sh
+sleep 180
+rbstat
+qsub go0-35.sh
+sleep 180
+rbstat
+qsub go2-35.sh
+sleep 180
+rbstat
+qsub go0-36.sh
+sleep 180
+rbstat
+qsub go2-36.sh
